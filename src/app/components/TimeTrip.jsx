be28edda835a2e41c3c90246804947289d9ae765
Rustam161 <rustambabaev161@gmail.com>
2026-04-17 05:28:43 +0300

    add prisma

--- src/app/components/TimeTrip.jsx
+++ src/app/components/TimeTrip.jsx
@@ -1,26 +1,16 @@
 
 import React from 'react';
-import { prisma } from '../admin/lib/prisma';
 import MapPage from './MapPage';
 
-export default async function TimeTrip() {
-    const city = await prisma.trip.findMany({
-        include: {
-            route: true,
-        },
-    });
-    console.log(city)
-    const from = city.filter(trip => trip.route.from === 'Ростов-на-дону');
-    const to = city.filter(trip => trip.route.to === 'Волгодонск');
-
+export default function TimeTrip() {
     const initialSelectedLocation = [
         {
             location: 'Ростов-на-дону',
-            time: from.flatMap(t => t.time || []),
+            time:['8:00'],
         },
         {
             location: 'Волгодонск',
-            time: to.flatMap(t => t.time || []),
+            time:['10:00']
         }
     ];
 
